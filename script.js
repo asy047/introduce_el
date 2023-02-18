32b75abda4361ffb8e986e2a1cd60331e0996ecf
An Seo-young <asy047@naver.com>
2023-02-19 00:24:05 +0900

    ver 0.2.1 대대적으로 걍 바꿈
    그.. 사람이 왜 디자인 툴이 따로 있고 코드 짜는게 따로 있는지 알아?.. 바로 .. 멀티가 안되기 때문이야.. 이제 안서영.. 머리에서 나오는 대로 디자인하고 코드 짜는건 너무 굳어서 못하겠다.. 안 한것도 있지만. . 하 ...  . .. 분발하자. . 일단 틀은 다 만들어 놨으니
    1. 컨텐츠 노다가 해서 우다다 올리고
    2. 애니메이션 머리 몇번 깨지면
    3. 완성 될 듯?
    +
    시간 남으면 팝업 슬라이드까지 완성하는 것으로 해보쟈
    어째 하는 코딩이 scss를 추가한거 빼고 똑같냐 ㅎㅎ;;; ..
    젠장

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-TypeHangul.type('.typing-text');
+console.log(`%c
+。　♡ 。　　♡。　　♡          
+♡。　＼　　｜　　／。　♡
+　  Welcome to EL 21
+♡。　／　　｜　　＼。　♡
+。　♡。 　　。　　♡。
 
-var loadingArea = document.querySelector("#loading");
-var loadingLogo = document.querySelector(".logo-area");
-
-loadingLogo.addEventListener('click', function() {
-  var mainC = document.querySelector('.mainC');
-  var subC = document.querySelector('.subC');
-  console.log(`。　♡ 。　　♡。　　♡
-  ♡。　＼　　｜　　／。　♡
-  　  Welcome to EL 21
-  ♡。　／　　｜　　＼。　♡
-  。　♡。 　　。　　♡。`);
-
-  mainC.classList.add('screen-animation');
-  setTimeout(() => {
-    loadingArea.style.display = 'none';
-    subC.classList.add('screen-animation');
-  }, 500);
-  setTimeout(() => {
-    mainC.style.opacity = 0;
-    subC.style.opacity = 0;
-  }, 1000);
-  setTimeout(() => {
-    mainC.style.display = 'none';
-    subC.style.display = 'none';
-  }, 1500);
-})
+`, "color: hotpink; font-size: 18px;");
+console.log(`
+／￣￣￣￣￣￣￣￣￣￣￣￣￣＼
+  코드 안 뜯어 봤음 좋겠다..
+＼＿＿＿＿＿＿＿＿＿＿＿＿＿／
+　　ｏ
+　　 。
+　　　｡
+　　∧＿∧
+　 (*　･ω･)
+＿(__つ/￣￣￣/_
+　　＼/　　　/
+`);
