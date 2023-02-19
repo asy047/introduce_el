dc1ec7a2e5fd748a5f54fccd72769d0aedf61a94
An Seo-young <asy047@naver.com>
2023-02-19 23:23:53 +0900

    test

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ console.log(`
 ＿(__つ/￣￣￣/_
 　　＼/　　　/
 `);
+console.log(`
+. . *
+* * . * . * . *
+. * . * . .
+. . *
+* * . * . * *
+. * .  * . .    21기 회장 안서영이.. 만듦...
+* * . * . * . *  Do It Myself..
+________________🚶🏻‍♂️_________
+
+`);
+
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', event => {
+    const accordionContent = header.nextElementSibling;
+
+    header.classList.toggle('active');
+    accordionContent.classList.toggle('active');
+
+    if (accordionContent.classList.contains('active')) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+    } else {
+      accordionContent.style.maxHeight = 0;
+    }
+  });
+});
